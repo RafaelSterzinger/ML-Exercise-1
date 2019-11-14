7522e257a8b5336d57786672ab9cd87de9f55593
Rafael <rafael.sterzinger@gmx.at>
2019-11-14 15:13:58 +0100

    add plots

--- bike_sharing.py
+++ bike_sharing.py
@@ -1,6 +1,6 @@
 import pandas as pd
-import matplotlib as plt
 import seaborn as sns
+from matplotlib import pyplot
 
 path = "./plots/bike_sharing/"
 
@@ -8,16 +8,19 @@ train = pd.read_csv("datasets/bike_sharing/bikeSharing.shuf.train.csv")
 test_data = pd.read_csv("datasets/bike_sharing/bikeSharing.shuf.test.csv")
 test_label = pd.read_csv("datasets/bike_sharing/bikeSharing.shuf.sampleSolution.csv")
 
-
-#Check for missing values
+# Check for missing values
 print(train.isnull().sum())
 
-#Histogram of target value
-plot = sns.distplot(train['cnt']).get_figure()
+fig, ax = pyplot.subplots(figsize=(6, 6))
+
+# Histogram of target value
+plot = sns.distplot(train['cnt'], ax=ax).get_figure()
 plot.savefig(path + "hist1.png")
 
+# Heatmap to check correlation
+correlation_matrix = train.corr().round(2)
+plot = sns.heatmap(correlation_matrix, ax=ax, linewidths=.5).get_figure()
+plot.savefig(path + "heatmap.png")
 
-
-#Linear Regression
-
+# Linear Regression
 
