0fb969a3ecae834795479a90ad79480771880ec4
Rafael <rafael.sterzinger@gmx.at>
2019-11-14 13:54:02 +0100

    add sample for automated tex creation

--- bike_sharing.py
+++ bike_sharing.py
@@ -1,9 +1,22 @@
 import pandas as pd
-import numpy as np
+import matplotlib as plt
+import seaborn as sns
 
-train = pd.read_csv("datasets/bike_sharing/bikeSharing.shuf.train.csv").to_numpy()
-test_data = pd.read_csv("datasets/bike_sharing/bikeSharing.shuf.test.csv").to_numpy()
-test_label = pd.read_csv("datasets/bike_sharing/bikeSharing.shuf.sampleSolution.csv").to_numpy
+path = "./plots/bike_sharing/"
+
+train = pd.read_csv("datasets/bike_sharing/bikeSharing.shuf.train.csv")
+test_data = pd.read_csv("datasets/bike_sharing/bikeSharing.shuf.test.csv")
+test_label = pd.read_csv("datasets/bike_sharing/bikeSharing.shuf.sampleSolution.csv")
+
+
+#Check for missing values
+print(train.isnull().sum())
+
+#Histogram of target value
+plot = sns.distplot(train['cnt']).get_figure()
+plot.savefig(path + "hist1.png")
+
+
 
 #Linear Regression
 
--- main.py
+++ main.py
@@ -1,1 +1,6 @@
-from sklearn import preprocessing
+from pdflatex import PDFLaTeX
+import bike_sharing
+import cpu_performance
+
+pdfl = PDFLaTeX.from_texfile("report.tex")
+pdf, log, completed_process = pdfl.create_pdf(keep_pdf_file=True, keep_log_file=False)
