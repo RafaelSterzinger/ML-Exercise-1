06c3beb454850624e8aef1d82d3b16510694a86b
Rafael <rafael.sterzinger@gmx.at>
2019-11-15 15:28:36 +0100

    add linear regression model

--- bike_sharing.py
+++ bike_sharing.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import seaborn as sns
+import numpy as np
 from matplotlib import pyplot
+from sklearn import linear_model
 
 path = "./plots/bike_sharing/"
 
@@ -11,16 +13,31 @@ test_label = pd.read_csv("datasets/bike_sharing/bikeSharing.shuf.sampleSolution.
 # Check for missing values
 print(train.isnull().sum())
 
-fig, ax = pyplot.subplots(figsize=(6, 6))
+fig, ax = pyplot.subplots(figsize=(10, 10))
 
 # Histogram of target value
 plot = sns.distplot(train['cnt'], ax=ax).get_figure()
 plot.savefig(path + "hist1.png")
 
 # Heatmap to check correlation
-correlation_matrix = train.corr().round(2)
+correlation_matrix = train.corr().round(2).abs()
+cnt_corr = correlation_matrix["cnt"].nlargest(8)
+
+# Selecting 5 largest without id, year
+attr = ["cnt", "temp", "atemp", "hum", "hr", "season"]
+correlation_matrix = train[attr].corr().round(2).abs()
 plot = sns.heatmap(correlation_matrix, ax=ax, linewidths=.5).get_figure()
 plot.savefig(path + "heatmap.png")
 
 # Linear Regression
+regr = linear_model.LinearRegression()
+bike_train_X = train[["temp", "atemp", "hum", "hr", "season"]]
+bike_train_y = train[["cnt"]]
+
+regr.fit(bike_train_X, bike_train_y)
+bike_y_pred = regr.predict(test_data[["temp", "atemp", "hum", "hr", "season"]])
+
+test_label = test_label.assign(cnt=bike_y_pred)
+print(test_label)
+test_label.to_csv(path + "linreg_pred.csv", index=False)
 
