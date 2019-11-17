fix scaling

knn_utils.py
@@ -12,7 +12,7 @@ from sklearn.model_selection import GridSearchCV
 
 def find_best_k(x_train, y_train, ks):
     """ Grid search, for efficient searching for k """
-    params = {'n_neighbors':ks}
+    params = {'n_neighbors': ks}
     knn = neighbors.KNeighborsRegressor()
     model = GridSearchCV(knn, params, cv=5)
     model.fit(x_train, y_train)
@@ -26,7 +26,7 @@ def get_ratio_missing(data, debug=False):
     data_num_missing = data_missing.sum()
     data_ratio_missing = data_num_missing / len(data)
     if debug:
-        print("Ratio of missing values:",data_ratio_missing)
+        print("Ratio of missing values:", data_ratio_missing)
     return data_ratio_missing
 
 
@@ -37,32 +37,31 @@ def trim_data(data, attributes):
 
 def create_cross_validation(data, target, test_size=0.3):
     """ create train and test set """
-    train , test = train_test_split(data, test_size = test_size)
+    train, test = train_test_split(data, test_size=test_size)
     x_train = train.drop(target, axis=1)
     y_train = train[target]
-    x_test = test.drop(target, axis = 1)
+    x_test = test.drop(target, axis=1)
     y_test = test[target]
     return x_train, y_train, x_test, y_test
 
 
-def scale_min_max(x_train, x_test):
+def scale_min_max(data):
     """ scaling features with MinMax """
     minmax_scaler = MinMaxScaler(feature_range=(0, 1))
-    x_train_minmax_scaled = pd.DataFrame(minmax_scaler.fit_transform(x_train))
-    x_test_minmax_scaled = pd.DataFrame(minmax_scaler.fit_transform(x_test))
-    return x_train_minmax_scaled, x_test_minmax_scaled
+    x_train_minmax_scaled = pd.DataFrame(minmax_scaler.fit_transform(data), index=data.index,
+                                         columns=data.columns)
+    return x_train_minmax_scaled
 
 
-def scale_standard(x_train,x_test):
+def scale_standard(data):
     """ scaling features with zscore """
     standard_scalar = StandardScaler()
-    x_train_zscore_scaled = pd.DataFrame(standard_scalar.fit_transform(x_train))
-    x_test_zscore_scaled = pd.DataFrame(standard_scalar.fit_transform(x_test))
-    return x_train_zscore_scaled, x_test_zscore_scaled
+    x_train_zscore_scaled = pd.DataFrame(standard_scalar.fit_transform(data), index=data.index,
+                                         columns=data.colums)
+    return x_train_zscore_scaled
 
 
-
-def find_best_rmse(name,x_train, y_train, x_test, y_test, k_max=23, metric='euclidean', plot=True, debug=False):
+def find_best_rmse(name, x_train, y_train, x_test, y_test, k_max=23, metric='euclidean', plot=True, debug=False):
     """ plot k vs rms and calculate best k """
     """
     :param k_max: maximum number of k to test for
@@ -74,32 +73,30 @@ def find_best_rmse(name,x_train, y_train, x_test, y_test, k_max=23, metric='eucl
     # Go through all k between k=1 and k=k_max-1 and find best_k and best_a
     # rsmes = np.zeros(k_max)  # Write rsmes for each k into here for plot to work...
 
-    rmse_val = [] # to store rmse values for different k
+    rmse_val = []  # to store rmse values for different k
     for k in range(1, k_max):
-        model = neighbors.KNeighborsRegressor(n_neighbors = k,metric=metric)
+        model = neighbors.KNeighborsRegressor(n_neighbors=k, metric=metric)
         model.fit(x_train, y_train)  # fit the model
-        pred=model.predict(x_test) # make prediction on test set
-        rsme = sqrt(mean_squared_error(y_test, pred)) # calculate rmse
+        pred = model.predict(x_test)  # make prediction on test set
+        rsme = sqrt(mean_squared_error(y_test, pred))  # calculate rmse
 
         if k == 1:
-            best_rmse=rsme
-            best_k=k
+            best_rmse = rsme
+            best_k = k
         elif rsme < best_rmse:
             best_rmse = rsme
             best_k = k
 
-        rmse_val.append(rsme) #store rmse values
+        rmse_val.append(rsme)  # store rmse values
         if debug:
             print('RMSE value for k=', k, 'is:', rsme)
 
     if plot:
         t = range(1, k_max)
-        plt.plot(t, rmse_val[0:k_max-1], '--', label=name)
+        plt.plot(t, rmse_val[0:k_max - 1], '--', label=name)
         plt.xticks(t)
         plt.xlabel('# neighbours (k)')
         plt.ylabel('rsme')
         plt.scatter(best_k, best_rmse)
         plt.legend()
-    return best_rmse,best_k
-
-
+    return best_rmse, best_k