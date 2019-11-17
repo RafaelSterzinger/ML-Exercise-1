5a33543a1630ff28a9c2fc834272d63bc5cb96c4
wedl <christian.stippel@gmail.com>
2019-11-17 02:48:39 +0100

    add mlp plot

--- student_performance_mlp.py
+++ student_performance_mlp.py
@@ -9,14 +9,15 @@ from sklearn.model_selection import train_test_split
 pd.set_option('display.expand_frame_repr', False)
 plt.rcParams["patch.force_edgecolor"] = True
 
-#%% enforce black border on histogram bins
+
 #%% read dataset
 data = pd.read_csv('datasets/student_performance/StudentPerformance.shuf.train.csv', index_col=0)
 
 #%%
 correlated_columns=highest_correlated_data_as_list(data,'Grade',10)
 correlated_columns.remove('Grade')
-print(correlated_columns)
 
 train, test = train_test_split(data, test_size=0.3)
-mlp(train, test, 'Grade', correlated_columns)
+rmse, model = mlp(train, test, 'Grade', correlated_columns)
+plot_loss(model)
+plt.show()
